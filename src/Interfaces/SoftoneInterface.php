9e63cb1e6d8517a71c477655df5d1b5366cc2e21
Aggelossik <a.sykamiotis@gmail.com>
2024-12-10 12:08:29 +0200

    readme update

--- src/Interfaces/SoftoneInterface.php
+++ src/Interfaces/SoftoneInterface.php
@@ -5,7 +5,9 @@ namespace Asikam\Softone\Interfaces;
 interface SoftoneInterface
 {
     public function setService($service):void;
-
     public function getService():string;
-
+    public function setBody():array;
+    public function getBody():array;
+    public function send():mixed;
+    public function toArray():array;
 }
