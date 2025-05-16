e83b6bff90c552e238fd0a4febe828b93ef0e368
Aggelossik <a.sykamiotis@gmail.com>
2025-05-16 10:52:31 +0300

    Align configuration array for consistent formatting
    Updated the spacing in the configuration array for better readability and consistency. This change improves code clarity and maintains a uniform style throughout the file.

--- src/config/softone.php
+++ src/config/softone.php
@@ -2,14 +2,14 @@
 
 return [
 
-    "COMPANY_AFM"                   => env('COMPANY_AFM'),
-    'SOFTONE_URL'                   => env('SOFTONE_URL'),
-    "SOFTONE_USER"                  => env('SOFTONE_USER'),
-    "SOFTONE_PASS"                  => env('SOFTONE_PASS'),
-    "SOFTONE_APPID"                 => env('SOFTONE_APPID'),
-    "SOFTONE_COMPANY"               => env('SOFTONE_COMPANY'),
-    "SOFTONE_BRANCH"                => env('SOFTONE_BRANCH'),
-    "SOFTONE_MODULE"                => env('SOFTONE_MODULE'),
-    "SOFTONE_REFID"                 => env('SOFTONE_REFID'),
+    "COMPANY_AFM"     => env('COMPANY_AFM'),
+    'SOFTONE_URL'     => env('SOFTONE_URL'),
+    "SOFTONE_USER"    => env('SOFTONE_USER'),
+    "SOFTONE_PASS"    => env('SOFTONE_PASS'),
+    "SOFTONE_APPID"   => env('SOFTONE_APPID'),
+    "SOFTONE_COMPANY" => env('SOFTONE_COMPANY'),
+    "SOFTONE_BRANCH"  => env('SOFTONE_BRANCH'),
+    "SOFTONE_MODULE"  => env('SOFTONE_MODULE'),
+    "SOFTONE_REFID"   => env('SOFTONE_REFID'),
 
 ];
